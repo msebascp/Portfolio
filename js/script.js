740c2b1429c1528664b87c96c23e0a23f0180fb0
sebas <manuel_sebastiancriollo@elcampico.org>
2023-06-27 11:46:12 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const changeTheme = () => {
+  document.documentElement.classList.toggle("dark");
+};
+
+window.onload = () => {
+  const buttonDark = document.querySelector("#buttonDarkTheme");
+  const buttonLight = document.querySelector("#buttonLightTheme");
+  const buttonMenu = document.querySelector("#buttonMenu");
+  const navMenu = document.querySelector("#navMenu");
+  // Cargamos el modo oscuro como predeterminado
+  document.documentElement.classList.add("dark");
+  buttonLight.style.display = "block";
+  let inicio = document.querySelector("#start");
+
+  buttonDark.onclick = () => {
+    changeTheme();
+    buttonLight.style.display = "block";
+    buttonDark.style.display = "none";
+    inicio.style.backgroundImage = "url('./media/bg.svg')";
+  };
+  buttonLight.onclick = () => {
+    changeTheme();
+    buttonLight.style.display = "none";
+    buttonDark.style.display = "block";
+    inicio.style.backgroundImage = "url('./media/bgLight.svg')";
+  };
+
+  buttonMenu.onclick = () => {
+    navMenu.classList.toggle("hidden");
+  };
+
+  buttonMenu.onblur = () => {
+    setTimeout(() => {
+      navMenu.classList.add("hidden");
+    }, 100);
+  };
+};

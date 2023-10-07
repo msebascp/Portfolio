24cc460eba5d04944b2772680c919c65ed5dfbc2
sebas <manuel_sebastiancriollo@elcampico.org>
2023-10-07 18:02:10 +0200

    improving the portfolio

--- js/script.js
+++ js/script.js
@@ -14,22 +14,70 @@ window.onload = () => {
     changeTheme();
     buttonLight.style.display = "block";
     buttonDark.style.display = "none";
-    inicio.style.backgroundImage = "url('./media/bg.svg')";
   };
   buttonLight.onclick = () => {
     changeTheme();
     buttonLight.style.display = "none";
     buttonDark.style.display = "block";
-    inicio.style.backgroundImage = "url('./media/bgLight.svg')";
   };
 
   buttonMenu.onclick = () => {
-    navMenu.classList.toggle("hidden");
+    buttonMenuOnClicked();
   };
 
   buttonMenu.onblur = () => {
-    setTimeout(() => {
-      navMenu.classList.add("hidden");
-    }, 100);
+    buttonMenuOnBlur();
   };
+};
+
+window.onscroll = () => {
+  changeClassHeaderOnScroll();
+};
+
+const classScroll = [
+  "dark:bg-blue-dark-2",
+  "bg-stone-300",
+  "border-b",
+  "dark:border-gray-300",
+  "border-blue-dark-2"
+];
+const classScroll0 = ["bg-transparent", "border-transparent"];
+
+const buttonMenuOnClicked = () => {
+  const header = document.querySelector("header");
+  const navMenu = document.querySelector("#navMenu");
+  navMenu.classList.toggle("hidden");
+  if (window.scrollY == 0) {
+    if (header.classList.contains(...classScroll)) {
+      header.classList.remove(...classScroll);
+    } else {
+      header.classList.add(...classScroll);
+    }
+  }
+};
+
+const buttonMenuOnBlur = () => {
+  const header = document.querySelector("header");
+  setTimeout(() => {
+    navMenu.classList.add("hidden");
+  }, 100);
+  if (window.scrollY == 0) {
+    header.classList.remove(...classScroll);
+  }
+};
+
+const changeClassHeaderOnScroll = () => {
+  const header = document.querySelector("header");
+  const navMenu = document.querySelector("#navMenu");
+  // Verificar si la página ha sido desplazada más allá del umbral
+  if (window.scrollY > 0) {
+    header.classList.add(...classScroll);
+    header.classList.remove(...classScroll0);
+  } else {
+    // Si estamos en la parte superior de la página
+    if (navMenu.classList.contains("hidden")) {
+      header.classList.remove(...classScroll);
+      header.classList.add(...classScroll0);
+    }
+  }
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
+const plugin = require("tailwindcss/plugin");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: "class",
-    content: ["./**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  darkMode: "class",
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-dark-1": "#2a2a2a",
+        "blue-dark-2": "#202020",
+        "color-secondary-1": "#6D5D6E",
+        "color-secondary-2": "#F4EEE0"
+      }
+    }
+  },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".text-balance": {
+          "text-wrap": "balance"
+        }
+      });
+    })
+  ]
+};
